addc96473118028d4b24c98dd25d6deb8f85c6e4
Fernando Moreira <fernandoepm.pro@gmail.com>
2021-06-21 17:56:50 -0300

    Inicializando projeto

---
+++ gymapp/Controllers/Onboarding/CriarContaViewController.swift
@@ -0,0 +1,22 @@
+//
+//  CriarContaViewController.swift
+//  gymapp
+//
+//  Created by Fernando Moreira on 21/06/21.
+//  Copyright © 2021 Fernando Moreira. All rights reserved.
+//
+
+import UIKit
+
+class CriarContaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.setNavigationBarHidden(false, animated: false)
+    }
+
+}
---
+++ gymapp/Controllers/Onboarding/LoginViewController.swift
@@ -0,0 +1,23 @@
+//
+//  LoginViewController.swift
+//  gymapp
+//
+//  Created by Fernando Moreira on 21/06/21.
+//  Copyright © 2021 Fernando Moreira. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.setNavigationBarHidden(true, animated: false)
+    }
+    
+
+}
