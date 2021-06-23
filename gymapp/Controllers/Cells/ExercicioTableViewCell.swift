3a82c0c230b58c6fcf915501c51dcbb453adc111
Fernando Moreira <fernandoepm.pro@gmail.com>
2021-06-22 23:45:00 -0300

    Listando exercícios dinamicamente

---
+++ gymapp/Controllers/Cells/ExercicioTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ExercicioTableViewCell.swift
+//  gymapp
+//
+//  Created by Fernando Moreira on 22/06/21.
+//  Copyright © 2021 Fernando Moreira. All rights reserved.
+//
+
+import UIKit
+
+class ExercicioTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var nomeExercicioLabel: UILabel!
+    @IBOutlet weak var observacaoExercicioLabel: UILabel!
+    @IBOutlet weak var exercicioImagem: UIImageView!
+    
+}
--- gymapp/Controllers/General/ExerciciosViewController.swift
+++ gymapp/Controllers/General/ExerciciosViewController.swift
@@ -7,20 +7,72 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseUI
+import FirebaseFirestore
 
 class ExerciciosViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     var treino: Dictionary<String, Any>!
+    var auth: Auth!
+    var db: Firestore!
+    var idUsuarioLogado: String!
+    var listaExercicios: [Dictionary<String, Any>] = []
     
+    @IBOutlet weak var tableViewExercicios: UITableView!
     @IBOutlet weak var nomeTreinoLabel: UILabel!
     @IBOutlet weak var descricaoTreinoLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.title = treino["nome"] as? String
+        nomeTreinoLabel.text = "Exercícios:"
+        /*
         nomeTreinoLabel.text = treino["nome"] as? String
         descricaoTreinoLabel.text = treino["descricao"] as? String
+        */
+                 
+        tableViewExercicios.separatorStyle = .none
+        
+        auth = Auth.auth()
+        db = Firestore.firestore()
+        
+        // Recupera id do usuário logado:
+        if let id = auth.currentUser?.uid {
+            self.idUsuarioLogado = id
+        }
+        
+    }
+    
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        recuperarExercicios()
+    }
+    
+    func recuperarExercicios() {
+        
+        self.listaExercicios.removeAll()
+        db.collection("usuarios")
+        .document(idUsuarioLogado)
+        .collection("treinos")
+        .document(treino["id"] as! String)
+        .collection("exercicios")
+            .getDocuments { (snapshotResult, error) in
                 
+                if let snapshot = snapshotResult {
+                    for document in snapshot.documents {
+                        
+                        let dadosExercicio = document.data()
+                        self.listaExercicios.append(dadosExercicio)
+                        
+                    }
+                    self.tableViewExercicios.reloadData()
+                }
+                
+        }
+        
     }
     
     
@@ -43,12 +95,36 @@ class ExerciciosViewController: UIViewController, UITableViewDelegate, UITableVi
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        let totalExercicios = self.listaExercicios.count
+        
+        if totalExercicios == 0 {
+            return 1
+        }
+        return totalExercicios
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let celula = tableView.dequeueReusableCell(withIdentifier: "exercicioCelula", for: indexPath)
+        let celula = tableView.dequeueReusableCell(withIdentifier: "exercicioCelula", for: indexPath) as! ExercicioTableViewCell
+        
+        if self.listaExercicios.count == 0 {
+            celula.nomeExercicioLabel.text = "Adicione um Exercício!"
+            celula.observacaoExercicioLabel.text = "Adicione um exercício para começar a treinar!"
+            celula.exercicioImagem.image = UIImage(named: "imagem-padrao-exercicio")
+            return celula
+        }
+        
+        let dadosExercicio = self.listaExercicios[indexPath.row]
+        
+        celula.nomeExercicioLabel.text = dadosExercicio["nome"] as? String
+        celula.observacaoExercicioLabel.text = dadosExercicio["observacao"] as? String
+        
+        if let imagemExercicio = dadosExercicio["urlImagem"] as? String {
+            celula.exercicioImagem.sd_setImage(with: URL(string: imagemExercicio), completed: nil)
+        } else {
+            celula.exercicioImagem.image = UIImage(named: "imagem-padrao-exercicio")
+        }
+        
         return celula
         
     }
